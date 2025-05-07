b543fb07a770e0d1ad168cc8d3666301fcc84b9e
nn1n09 <skkim05290@gmail.com>
2025-05-07 17:47:23 +0900

    변경

--- test.py
+++ test.py
@@ -1,6 +1,11 @@
 import streamlit as st
 
-st.title('title')
-st.header('this is header')
-st.subheader('this is subheader')
-   
+col1, col2 = st.columns([2,3])
+with col1:
+    st.title('here is col1')
+with col2:
+    st.title('here is col2')
+    st.checkbox('this is checkbox in col2')
+
+col1.subheader('i am col1 subheader')
+col2.checkbox('check in 2')
